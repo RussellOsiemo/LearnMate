85ac3500f620237207df559598ad374731bef511
Russel Osiemo <86182360+RussellOsiemo@users.noreply.github.com>
2021-09-11 22:38:42 +0300

    Style Commit

--- search.js
+++
@@ -1,9 +0,0 @@
-const searchBar = document.querySelector(".searchfield input"),
-searchBtn = document.querySelector(".searchfield button");
-
-searchBtn.onclick = () =>{
-    searchBar.classList.toggle("active");
-     searchBar.focus();
-     searchBtn.classList.toggle("active");
-
-} 
